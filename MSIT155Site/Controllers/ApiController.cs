226324eced0e306e8d431ab29953d69a9e87ff34
Tony <ispan0318@gmail.com>
2024-01-29 19:10:26 +0800

    加入專案檔案。

---
+++ MSIT155Site/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSIT155Site.Models;
+
+namespace MSIT155Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
